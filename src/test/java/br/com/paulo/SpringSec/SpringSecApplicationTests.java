658703ef1116468b57ad3de7e3916d7f25045a65
Galleazzo <paulogalleazzo1@gmail.com>
2023-01-26 21:05:15 -0300

    criar repo

---
+++ src/test/java/br/com/paulo/SpringSec/SpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulo.SpringSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
